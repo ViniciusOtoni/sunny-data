704874712b8e701852189fccfcb3e1f29f65d946
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-07-05 12:20:14 -0300

    Corrigindo "warnings" emitidos pelo Terraform por conta do Provider não ter sido explicitamente especificado em cada módulo e alterando YAML da esteira.

--- infra/modules/databricks_workspace/main.tf
+++ infra/modules/databricks_workspace/main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    } 
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.0"
+    }
+  }
+  }
+}
+
 resource "azurerm_databricks_workspace" "this" {
   name                = var.workspace_name
   location            = var.location
--- infra/modules/storage_account/main.tf
+++ infra/modules/storage_account/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
 resource "azurerm_storage_account" "this" {
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
