e97baf389be5485b8dfa8d3ec1753812112d7c26
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-07-05 12:26:09 -0300

    "ajuste" retirando o provider do databricks no resource responsável por gerar a workspace... Ele já está sendo específicado no meu main.tf!

--- infra/modules/databricks_workspace/main.tf
+++ infra/modules/databricks_workspace/main.tf
@@ -3,12 +3,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
-    } 
-    databricks = {
-      source  = "databricks/databricks"
-      version = "~> 1.0"
     }
-  }
   }
 }
 
