80b8c72fc699814f3923fdc32231236e8ab6d2f0
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-07-05 12:30:54 -0300

    corrigindo problema das credencias no provider "azuread"... Assim não tendo nenhum tipo de fallback do (az -login)

--- infra/providers.tf
+++ infra/providers.tf
@@ -27,4 +27,6 @@ provider "azurerm" {
 provider "azuread" {
   alias     = "admin"
   tenant_id = var.tenant_id
+  client_id       = var.admin_client_id
+  client_secret   = var.admin_client_secret
 }
