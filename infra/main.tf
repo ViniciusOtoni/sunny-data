fc496d83e55914a8c0a8da08b848d2b33beef7ce
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-07-09 22:54:10 -0300

    ajustes no alinhamento/dependencias dos processos para criação dos respectivos RGs

--- infra/main.tf
+++ infra/main.tf
@@ -15,8 +15,12 @@ terraform {
   }
 }
 
-# Módulo para criar a SPN
+# Recupera informações da subscription
+data "azurerm_subscription" "primary" {
+  provider = azurerm.admin
+}
 
+# Módulo para criar a SPN dinâmica
 module "service_principal" {
   source    = "./modules/service_principal"
   tenant_id = var.tenant_id
@@ -26,39 +30,21 @@ module "service_principal" {
   }
 }
 
-# RG para recursos persistentes
+# Resource Group para SPN + Key Vault
 resource "azurerm_resource_group" "rg_core" {
   name     = "rg-medalforge-core"
   location = "brazilsouth"
   provider = azurerm.admin
 }
 
-# RG para infraestrutura do projeto
+# Resource Group para todos os outros recursos da infraestrutura
 resource "azurerm_resource_group" "rg_datalake" {
   name     = "rg-medalforge-datalake"
   location = "brazilsouth"
   provider = azurerm.admin
 }
 
-# Subscription atual
-data "azurerm_subscription" "primary" {}
-
-# Permissões para a SPN 
-resource "azurerm_role_assignment" "spn_contributor_datalake" {
-  provider             = azurerm.admin
-  scope                = azurerm_resource_group.rg_datalake.id
-  role_definition_name = "Contributor"
-  principal_id         = module.service_principal.spn_object_id
-}
-
-resource "azurerm_role_assignment" "spn_reader_subscription" {
-  provider             = azurerm.admin
-  scope                = data.azurerm_subscription.primary.id
-  role_definition_name = "Reader"
-  principal_id         = module.service_principal.spn_object_id
-}
-
-# Key Vault no RG Core
+# Key Vault no RG core, provisionado pelo admin
 module "key_vault" {
   source                  = "./modules/key_vault"
   name                    = "akv-medalforge-rbac-core"
@@ -68,20 +54,46 @@ module "key_vault" {
   bootstrap_spn_object_id = var.bootstrap_spn_object_id
   spn_client_id           = module.service_principal.spn_client_id
   spn_client_secret       = module.service_principal.spn_client_secret
-  providers               = { azurerm = azurerm.admin }
+
+  providers = {
+    azurerm = azurerm.admin
+  }
 }
 
-# Storage Account no RG Datalake
+# Role Assignment: SPN como Reader na subscription
+resource "azurerm_role_assignment" "spn_reader_subscription" {
+  provider             = azurerm.admin
+  scope                = data.azurerm_subscription.primary.id
+  role_definition_name = "Reader"
+  principal_id         = module.service_principal.spn_object_id
+}
+
+# Role Assignment: SPN como Contributor no RG datalake
+resource "azurerm_role_assignment" "spn_contributor_datalake" {
+  provider             = azurerm.admin
+  scope                = azurerm_resource_group.rg_datalake.id
+  role_definition_name = "Contributor"
+  principal_id         = module.service_principal.spn_object_id
+
+  depends_on = [
+    azurerm_resource_group.rg_datalake
+  ]
+}
+
+# Storage Account no RG datalake
 module "storage_account" {
   source                = "./modules/storage_account"
-  providers             = { azurerm = azurerm.spn }
   resource_group_name   = azurerm_resource_group.rg_datalake.name
   location              = azurerm_resource_group.rg_datalake.location
   storage_account_name  = "medalforgestorage"
   container_name        = "raw"
+
+  providers = {
+    azurerm = azurerm.spn
+  }
 }
 
-# Databricks Workspace no RG Datalake
+# Databricks Workspace no RG datalake
 module "databricks_workspace" {
   source              = "./modules/databricks_workspace"
   workspace_name      = "medalforge-databricks"
