42a55328d2d6bf5f33c7f6490f5b4a8ce8101c03
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-07-18 17:17:09 -0300

    adicionando output de workspace_name

--- infra/main.tf
+++ infra/main.tf
@@ -124,7 +124,7 @@ resource "azurerm_databricks_access_connector" "uc" {
   name                = "ac-medalforge"
   resource_group_name = azurerm_resource_group.rg_datalake.name
   location            = azurerm_resource_group.rg_datalake.location
-  workspace_id   = module.workspace_create.workspace_id
+  workspace_name      = module.workspace_create.workspace_name
   identity {
     type = "SystemAssigned"
   }
