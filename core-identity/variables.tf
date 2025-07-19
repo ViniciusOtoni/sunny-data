86344b46db5c45ea0419e52fbe66691cc5a6df12
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-07-19 14:09:38 -0300

    Alterando identação!

--- core-identity/variables.tf
+++ core-identity/variables.tf
@@ -1,15 +1,51 @@
-# Credenciais / IDs
+# Credenciais / IDs    
 
-variable "subscription_id"         { type = string }
-variable "tenant_id"               { type = string }
-variable "admin_client_id"         { type = string }
-variable "admin_client_secret"     { type = string }
-variable "bootstrap_spn_object_id" { type = string }
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
 
-# Convenções de nomes 
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
 
-variable "location"         { type = string  default = "brazilsouth" }
-variable "rg_core_name"     { type = string  default = "rg-medalforge-core" }
-variable "rg_datalake_name" { type = string  default = "rg-medalforge-datalake" }
-variable "key_vault_name"   { type = string  default = "akv-medalforge-rbac-core" }
+variable "admin_client_id" {
+  type        = string
+  description = "Client ID da SPN admin (Owner)"
+}
 
+variable "admin_client_secret" {
+  type        = string
+  description = "Client Secret da SPN admin"
+  sensitive   = true
+}
+
+variable "bootstrap_spn_object_id" {
+  type        = string
+  description = "Object ID da SPN bootstrap criada manualmente"
+}
+
+
+# Convenções de valores
+
+variable "location" {
+  type        = string
+  default     = "brazilsouth"
+  description = "Região padrão dos recursos"
+}
+
+variable "rg_core_name" {
+  type        = string
+  default     = "rg-medalforge-core"
+}
+
+variable "rg_datalake_name" {
+  type        = string
+  default     = "rg-medalforge-datalake"
+}
+
+variable "key_vault_name" {
+  type        = string
+  default     = "akv-medalforge-rbac-core"
+}
