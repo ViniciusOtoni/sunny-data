07dd508ca52e9d26c859ec2a6b30fd2b1c093884
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-07-21 00:20:28 -0300

    fixing

--- landing-zone/outputs.tf
+++ landing-zone/outputs.tf
@@ -1,3 +1,3 @@
 output "state_sa_name"    { value =  azurerm_storage_account.tfstate.name }
 output "state_rg_name"    { value =  data.terraform_remote_state.identity.outputs.rg_core_name }
-output "rg_datalake_name" { value = data.terraform_remote_state.identity.outputs.rg_datalake }
+output "rg_datalake_name" { value =  data.terraform_remote_state.identity.outputs.rg_datalake_name }
