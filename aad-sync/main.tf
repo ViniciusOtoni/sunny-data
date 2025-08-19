88945cee9f47aa1ac4ab58be585f0f938735e447
ViniciusOtoni <viniciusotonimsv@gmail.com>
2025-08-18 21:05:38 -0300

    fixing providers

--- aad-sync/main.tf
+++ aad-sync/main.tf
@@ -7,13 +7,13 @@ resource "azuread_group" "aad_groups" {
   for_each         = local.groups
   display_name     = each.key
   security_enabled = true
-  providers        = { azuread = azuread.admin }
+  provider = azuread.admin
 }
 
 # 2) Enterprise App “Azure Databricks SCIM Provisioning Connector”
 data "azuread_application_template" "dbx_scim" {
   display_name = "Azure Databricks SCIM Provisioning Connector"
-  providers    = { azuread = azuread.admin }
+  provider = azuread.admin
 }
 
 resource "azuread_application" "scim_app" {
@@ -23,13 +23,13 @@ resource "azuread_application" "scim_app" {
                   enterprise = true 
                   gallery = true 
                 }
-  providers = { azuread = azuread.admin }
+  provider = azuread.admin  
 }
 
 resource "azuread_service_principal" "scim_sp" {
   client_id                    = azuread_application.scim_app.client_id
   app_role_assignment_required = false  # permite assignment sem exigência prévia
-  providers = { azuread = azuread.admin }
+  provider = azuread.admin
 }
 
 # 3) Segredos do job de provisionamento (SCIM BaseAddress + SecretToken)
@@ -47,7 +47,7 @@ resource "azuread_synchronization_secret" "scim_creds" {
     key = "SyncAll"     
     value = "false" 
   } # sincroniza só “assigned”
-  providers = { azuread = azuread.admin }
+  provider = azuread.admin
 }
 
 # 4) Escopo: atribuir grupos ao Enterprise App (role “User”)
@@ -56,7 +56,7 @@ resource "azuread_app_role_assignment" "assign_groups" {
   principal_object_id = each.value.object_id
   resource_object_id  = azuread_service_principal.scim_sp.object_id
   app_role_id         = azuread_service_principal.scim_sp.app_role_ids["User"]
-  providers           = { azuread = azuread.admin }
+  provider            = azuread.admin
   depends_on          = [azuread_synchronization_secret.scim_creds]
 }
 
@@ -65,7 +65,7 @@ resource "azuread_synchronization_job" "scim_job" {
   service_principal_id = azuread_service_principal.scim_sp.id
   template_id          = "databricks"
   enabled              = true
-  providers            = { azuread = azuread.admin }
+  provider             = azuread.admin
   depends_on           = [azuread_app_role_assignment.assign_groups]
 }
 
